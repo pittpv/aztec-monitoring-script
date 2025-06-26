d2ca331fab76d92c37ca4e3cd5967dc0f0d6d83d
pittpv <pittpv@gmail.com>
2025-06-26 10:19:39 +0300

    release v.1.8.0

--- other/logo.sh
+++ other/logo.sh
@@ -14,21 +14,26 @@ print_colored "██║  ██║███████╗   ██║   ██
 print_colored "╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝ ╚═════╝"
 echo
 
+# Цвета
+b=$'\033[34m' # Blue
+y=$'\033[33m' # Yellow
+r=$'\033[0m'  # Reset
+
 # Информация в рамке
 info_lines=(
-  "✦ Made by Pittpv                                            "
-  "✦ Feedback & Support in Tg: https://t.me/+DLsyG6ol3SFjM2Vk  "
-  "✦ Donate                                                    "
+  "✦ Made by Pittpv"
+  "✦ Feedback & Support in Tg: https://t.me/+DLsyG6ol3SFjM2Vk"
+  "✦ Donate"
   "  EVM: 0x4FD5eC033BA33507E2dbFE57ca3ce0A6D70b48Bf"
   "  SOL: C9TV7Q4N77LrKJx4njpdttxmgpJ9HGFmQAn7GyDebH4R"
 )
 
-# Вычисляем максимальную длину строки
+# Вычисляем максимальную длину строки (с учётом Unicode-символов)
 max_len=0
 for line in "${info_lines[@]}"; do
-  # Не учитываем цвета при подсчёте длины
   clean_line=$(echo -e "$line" | sed -E 's/\x1B\[[0-9;]*[mK]//g')
-  [ ${#clean_line} -gt $max_len ] && max_len=${#clean_line}
+  line_length=$(echo -n "$clean_line" | wc -m)
+  [ "$line_length" -gt "$max_len" ] && max_len=$line_length
 done
 
 # Строим рамки
@@ -38,9 +43,10 @@ bottom_border="╚$(printf '═%.0s' $(seq 1 $((max_len + 2))))╝"
 # Печать рамки
 echo -e "${b}${top_border}${r}"
 for line in "${info_lines[@]}"; do
-  # Выводим строку с цветом, но выравниваем по чистой длине
   clean_line=$(echo -e "$line" | sed -E 's/\x1B\[[0-9;]*[mK]//g')
-  printf "${b}║ ${y}%-*s ${b}║\n" "$max_len" "$line"
+  line_length=$(echo -n "$clean_line" | wc -m)
+  padding=$((max_len - line_length))
+  printf "${b}║ ${y}%s%*s ${b}║\n" "$line" "$padding" ""
 done
 echo -e "${b}${bottom_border}${r}"
 echo
