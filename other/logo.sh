90b2b1fee30d9ebc67fcdf13c30785927149fd80
pittpv <pittpv@gmail.com>
2025-06-26 11:03:21 +0300

    release v.1.8.0

--- other/logo.sh
+++ other/logo.sh
@@ -1,3 +1,19 @@
+function print_colored() {
+  local b=$'\033[34m' # Blue
+  local y=$'\033[33m' # Yellow
+  local r=$'\033[0m'  # Reset
+  echo -e "${b}$(echo "$1" | sed -E "s/(█+)/${y}\1${b}/g")${r}"
+}
+
+echo
+print_colored " █████╗ ███████╗████████╗███████╗ ██████╗"
+print_colored "██╔══██╗╚══███╔╝╚══██╔══╝██╔════╝██╔════╝"
+print_colored "███████║  ███╔╝    ██║   █████╗  ██║"
+print_colored "██╔══██║ ███╔╝     ██║   ██╔══╝  ██║"
+print_colored "██║  ██║███████╗   ██║   ███████╗╚██████╗"
+print_colored "╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝ ╚═════╝"
+echo
+
 # Цвета
 b=$'\033[34m' # Blue
 y=$'\033[33m' # Yellow
