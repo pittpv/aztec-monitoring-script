5888f1e163da7b2d88e973c5861eace24c68853a
pittpv <pittpv@gmail.com>
2025-09-19 08:12:48 +0300

    release v.2.0.0

--- other/check-validator.sh
+++ other/check-validator.sh
@@ -216,7 +216,7 @@ init_languages "$1"
 
 #ROLLUP_ADDRESS="0x1bb7836854ce5dc7d84a32cb75c7480c72767132"
 ROLLUP_ADDRESS="0x29fa27e173f058d0f5f618f5abad2757747f673f"
-GSE_ADDRESS="0xb088487022867ed1127ba6eb9b2e8040ceda312e"
+GSE_ADDRESS="0x67788e5083646ccedeeb07e7bc35ab0d511fc8b9"
 QUEUE_URL="https://dashtec.xyz/api/validators/queue"
 MONITOR_DIR="/root/aztec-monitor-agent"
 
